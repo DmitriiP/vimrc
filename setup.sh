1be46fe98fe3c30f998ce50595072be04eb9f14b
Dmitrii Prihodco <dmitrii.prihodco@gmail.com>
2017-10-22 14:37:09 +0300

    Most accustomed font for me.

--- setup.sh
+++ setup.sh
@@ -17,10 +17,20 @@ echo `command_exists git`
 if command_exists git; then
     echo "git exists yay!"
 fi
+
 echo "Starting basic setup."
+
+git clone https://github.com/abertsch/Menlo-for-Powerline.git
+if [[ "$unamestr" != "Darwin" ]]; then
+    cp Menlo-for-Powerline/* ~/Library/Fonts
+else
+    cp Menlo-for-Powerline/* ~/.local/share/fonts
+fi
+
 git clone https://github.com/powerline/fonts.git
 fonts/install.sh
 rm -rf fonts
+
 git submodule update --init --recursive
 cp .vimrc ~
 cp .gvimrc ~
